c6f533488fea9e4d30766cf94132a872250f7754
Tilak dave <davetilak003@gmail.com>
2022-01-29 14:47:25 +0530

    main google ads code

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -19,10 +19,15 @@ const config = {
   trailingSlash: false,
   scripts: [
     {
+      src: "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3981636219602601",
+      crossorigin: "anonymous",
+      async: true,
+    },
+    {
       "data-ad-client": "ca-pub-3981636219602601",
       src: 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js',
       async: true,
-    }
+    },
   ],
 
   themeConfig:
