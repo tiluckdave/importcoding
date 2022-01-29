bef102ba760d403917debcba5df3f2f169ae9252
Tilak dave <davetilak003@gmail.com>
2022-01-29 14:53:37 +0530

    ads code

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -28,6 +28,12 @@ const config = {
       src: 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js',
       async: true,
     },
+    {
+      client: "ca-pub-3981636219602601",
+      src: 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js',
+      async: true,
+      crossorigin: "anonymous",
+    },
   ],
 
   themeConfig:
